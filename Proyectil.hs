83f9b8d95805cdea99908347245ab80288c40be7
AndresMardonesUACH <andymardones1@gmail.com>
2024-11-02 23:43:39 -0300

    Aleatoriedad añadida

--- Proyectil.hs
+++ Proyectil.hs
@@ -22,7 +22,7 @@ getHitBox (x, y) = ((x-11, x+11), (largoMatrix - y - 2, largoMatrix - y))
 --Función que determina si un barco fue impactado o no
 fueImpactado :: (Int, Int) -> ((Int, Int), (Int, Int)) -> Bool
 fueImpactado (x, y) ((lim1, lim2), (lim3, lim4))
-  | (x > lim1 && x < lim2) && (y > lim3 && y < lim4) = True
+  | (x >= lim1 && x <= lim2) && (y >= lim3 && y <= lim4) = True
   | otherwise = False
 
 -- Función que determina el ángulo según el modo del cañon
@@ -78,8 +78,8 @@ calculaPos v0 angle t x0 y0 = (x, y)
     y = round $ (fromIntegral y0) + (fromIntegral v0) * sin theta * t - 0.5 * g * t^2
 
 -- Simular la trayectoria paso a paso con posición inicial (x0, y0)
-simulateTrajectory :: Canon (Int, Int, Int, Int, Int) -> Int -> Int -> (Int, Int) -> [[Char]] -> Double -> IO (Canon (Int, Int, Int, Int, Int))
-simulateTrajectory canon2 v0 degree (x0, y0) matrix maxTime = simulateStep canon2 0
+simulateTrajectory :: Canon (Int, Int, Int, Int, Int) -> Int -> Int -> (Int, Int) -> [[Char]] -> Double -> StdGen -> IO (Canon (Int, Int, Int, Int, Int))
+simulateTrajectory canon2 v0 degree (x0, y0) matrix maxTime gen = simulateStep canon2 0
   where
     simulateStep canon2 t
       | t > maxTime = return canon2  -- Termina la simulación y devuelve el estado final del cañón
@@ -88,7 +88,7 @@ simulateTrajectory canon2 v0 degree (x0, y0) matrix maxTime = simulateStep canon
           if x >= 0 && x < length (head matrix) && y >= 0 && y < length matrix
             then do
               if (fueImpactado (x,y) (getHitBox (getX canon2, getY canon2))) then do 
-                                let newCanon = canon2 >>= impactar (mkStdGen 2)
+                                let newCanon = canon2 >>= impactar gen
                                 return newCanon
               else do
                 let newMatrix = markMatrix matrix (x, length matrix - y - 1)
@@ -99,8 +99,8 @@ simulateTrajectory canon2 v0 degree (x0, y0) matrix maxTime = simulateStep canon
               if (y >= 0) then simulateStep canon2 (t + 0.1) else return canon2  -- Si la posición de la bala sale de los límites, termina la simulación y devuelve el cañón
 
 
-dispararProyectil:: Canon (Int, Int, Int, Int, Int) -> Canon (Int, Int, Int, Int, Int)  -> Char -> [[Char]]-> IO (Canon(Int, Int, Int, Int, Int))
-dispararProyectil canon1 canon2 h matriz = simulateTrajectory canon2 35 angle cannonCoord matriz 50
+dispararProyectil:: Canon (Int, Int, Int, Int, Int) -> Canon (Int, Int, Int, Int, Int)  -> Char -> [[Char]]-> StdGen -> IO (Canon(Int, Int, Int, Int, Int))
+dispararProyectil canon1 canon2 h matriz gen = simulateTrajectory canon2 35 angle cannonCoord matriz 50 gen
   where
     cannonCoord = getCannonCoord (getX canon1, getY canon1) h (getAngle canon1)
-    angle = getDegree h (getAngle canon1)
+    angle = (getDegree h (getAngle canon1)) +  (fst (randomR (-5,5) gen :: (Int, StdGen)))
--- main.hs
+++ main.hs
@@ -4,6 +4,7 @@ import System.Process (system)  -- Para limpiar la consola
 import Graphics
 import Proyectil
 import Tools
+import System.Random
 
 --------------------------------------------------------------------------------------------------
 -- CONSTANTES
@@ -12,8 +13,8 @@ import Tools
 largoMatrix :: Int
 largoMatrix = 50
 
-loop :: (Canon (Int, Int, Int, Int, Int)) -> (Canon (Int, Int, Int, Int, Int)) -> [[Char]] -> IO ()
-loop canon1 canon2 matrix = do
+loop :: (Canon (Int, Int, Int, Int, Int)) -> (Canon (Int, Int, Int, Int, Int)) -> [[Char]] -> StdGen ->  IO ()
+loop canon1 canon2 matrix gen = do
     let newMatrix = actualizaMatriz matrix (getX canon1, getY canon1) (getAngle canon1) 'r' --dibuja barco izquierdo
     let newMatrix2 = actualizaMatriz newMatrix (getX canon2, getY canon2) (getAngle canon2) 'l' --dibuja barco derecho
     printMatrix newMatrix2 --imprime la matriz
@@ -23,43 +24,46 @@ loop canon1 canon2 matrix = do
     case c of
         'a' -> do
             let newCanon = if (getX canon1) > 12 then  canon1 >>= (moveX (-1)) else canon1 -- Mover barco izquierdo hacia la izquierda
-            loop newCanon canon2 matrix
+            loop newCanon canon2 matrix gen
         'd' -> do
             let newCanon = if (getX canon1) < 59 then  canon1 >>= (moveX 1) else canon1 -- Mover barco izquierdo hacia la derecha
-            loop newCanon canon2 matrix
+            loop newCanon canon2 matrix gen
         'w' -> do
             let newCanon = if (getAngle canon1) < 3 then  canon1 >>= (moveAngle 1) else canon1 -- Mover ángulo de cañón derecho hacia arriba
-            loop  newCanon canon2 matrix
+            loop  newCanon canon2 matrix gen
         's' -> do
             let newCanon = if (getAngle canon1) > 0 then  canon1 >>= (moveAngle (-1)) else canon1 -- Mover ángulo de cañón derecho hacia abajo
-            loop newCanon canon2 matrix
+            loop newCanon canon2 matrix gen
         'j' -> do
             let newCanon = if (getX canon2) > 116 then  canon2 >>= (moveX (-1)) else canon2 -- Mover barco derecho hacia la izquierda
-            loop canon1 newCanon matrix
+            loop canon1 newCanon matrix gen
         'l' -> do
             let newCanon = if (getX canon2) < 156 then  canon2 >>= (moveX 1) else canon2 -- Mover barco derecho hacia la derecha
-            loop canon1 newCanon matrix
+            loop canon1 newCanon matrix gen
         'i' -> do
             let newCanon = if (getAngle canon2) < 3 then canon2 >>= (moveAngle 1) else canon2 -- Mover ángulo de cañón izquierdo hacia arriba
-            loop canon1 newCanon matrix
+            loop canon1 newCanon matrix gen
         'k' -> do
             let newCanon = if (getAngle canon2) > 0 then canon2 >>= (moveAngle (-1)) else canon2 -- Mover ángulo de cañón izquierdo hacia abajo
-            loop canon1 newCanon matrix
+            loop canon1 newCanon matrix gen
         'p' -> do
-            newCanon <- dispararProyectil canon1 canon2 'r' newMatrix2 -- Barco izquierdo dispara proyectil
-            loop canon1 newCanon matrix
+            newCanon <- dispararProyectil canon1 canon2 'r' newMatrix2 gen -- Barco izquierdo dispara proyectil
+            let nextGen = snd (random gen :: (Int, StdGen))
+            loop canon1 newCanon matrix nextGen
         'o' -> do
-            newCanon <- dispararProyectil canon2 canon1 'l' newMatrix2 -- Barco derecho dispara proyectil
-            loop newCanon canon2 matrix
+            newCanon <- dispararProyectil canon2 canon1 'l' newMatrix2 gen -- Barco derecho dispara proyectil
+            let nextGen = snd (random gen :: (Int, StdGen))
+            loop newCanon canon2 matrix nextGen
         'q' -> return ()                -- Salir del bucle
-        _  -> loop canon1 canon2 matrix -- Ignorar otras teclas y repetir
+        _  -> loop canon1 canon2 matrix gen-- Ignorar otras teclas y repetir
 ---------------------------------------------------------------------------
 main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering -- Evitar el buffering en la entrada
     hSetEcho stdin False            -- Desactivar la impresión de teclas
     putStrLn "Presiona teclas (q para salir):"
+    gen <- getStdGen
     let canon1 = Canon (30, 100, 20, 43, 0)
     let canon2 = Canon (30, 100, 150, 43, 0)
     let matrix = initialMatrix    -- Se invoca la matriz inicial (Graphics.hs)
-    loop canon1 canon2 matrix              -- Coordenadas iniciales
+    loop canon1 canon2 matrix gen           -- Coordenadas iniciales
